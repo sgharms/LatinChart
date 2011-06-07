3b6dea210510ec51d1ac55bccab54bdd6b73c8b5
Steven G. Harms <sgharms@stevengharms.com>
2011-06-06 18:27:51 -0700

    Provides basic functionality and lookup capability

--- lib/latinchart.rb
+++ lib/latinchart.rb
@@ -1,3 +1,14 @@
+# encoding: utf-8
 module Latinchart
-  # Your code goes here...
+  MAPPING = {
+   "1"   => Linguistics::Latin::Verb::LatinVerb.new(%q(amō   amāre  amāvī  amatum)), 
+   "2"   => Linguistics::Latin::Verb::LatinVerb.new(%q(moneō monēre monvī  monitum)) , 
+   "3"   => Linguistics::Latin::Verb::LatinVerb.new(%q(agō   agere  ēgī    actum)), 
+   "4"   => Linguistics::Latin::Verb::LatinVerb.new(%q(capiō capere cēpī   captum)), 
+   "3io" => Linguistics::Latin::Verb::LatinVerb.new(%q(audiō audīre audīvī auditum))   
+  }
+
+  def self.razzle
+    puts 'you have been razzled'
+  end
 end
